449e5b9d100bd888bb54623f712bfbd54c2dd302
saragirard <sarag.bsa@gmail.com>
2022-05-06 14:48:32 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+
+}
+// <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+//     <div class="container-fluid">
+//         <a class="navbar-brand" href="#">Sara Girard</a>
+//         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+//             <span class="navbar-toggler-icon"></span>
+//         </button>
+//         <div class="collapse navbar-collapse" id="navbarColor01">
+//             <ul class="navbar-nav me-auto">
+//                 <li class="nav-item">
+//                     <a class="nav-link" href="{{ url_for('index') }}">Home
+//                     </a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a class="nav-link" href="{{ url_for('resume') }}">Resume</a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a class="nav-link" href="{{ url_for('portfolio') }}">Portfolio</a>
+//                 </li>
+//             </ul>
+//         </div>
+//     </div>
+// </nav>
